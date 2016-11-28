e78750fcbb6d68993b21984abf5a5cface14f739
harishsune <harishsune@hotmail.com>
2016-11-29 01:24:58 +0530

    RnD on Custom Controls

---
+++ TreeListView_Sources1/TreeListView/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.TreeListViewItemCollection.TreeListViewItemCollectionComparer treeListViewItemCollectionComparer1 = new System.Windows.Forms.TreeListViewItemCollection.TreeListViewItemCollectionComparer();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.treeListView1 = new System.Windows.Forms.TreeListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "computer.png");
+            // 
+            // treeListView1
+            // 
+            this.treeListView1.AutoArrange = false;
+            this.treeListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            treeListViewItemCollectionComparer1.Column = 0;
+            treeListViewItemCollectionComparer1.SortOrder = System.Windows.Forms.SortOrder.Ascending;
+            this.treeListView1.Comparer = treeListViewItemCollectionComparer1;
+            this.treeListView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeListView1.FullRowSelect = false;
+            this.treeListView1.GridLines = true;
+            this.treeListView1.HideSelection = false;
+            this.treeListView1.LargeImageList = this.imageList1;
+            this.treeListView1.Location = new System.Drawing.Point(0, 0);
+            this.treeListView1.Name = "treeListView1";
+            this.treeListView1.PlusMinusLineColor = System.Drawing.Color.Black;
+            this.treeListView1.Size = new System.Drawing.Size(284, 261);
+            this.treeListView1.SmallImageList = this.imageList1;
+            this.treeListView1.TabIndex = 0;
+            this.treeListView1.UseCompatibleStateImageBehavior = false;
+            this.treeListView1.ItemActivate += new System.EventHandler(this.treeListView1_ItemActivate);
+            this.treeListView1.SelectedIndexChanged += new System.EventHandler(this.treeListView1_SelectedIndexChanged);
+            this.treeListView1.Click += new System.EventHandler(this.treeListView1_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "C1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.treeListView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeListView treeListView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ TreeListView_Sources1/TreeListView/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TreeListViewItem oItem = new TreeListViewItem("Harish", 0);
+            oItem.Name = "Harish";
+            treeListView1.Items.Add(oItem);
+            treeListView1.Items["Harish"].Items.Add("XYZ");
+
+            TreeListViewItem oItem1 = new TreeListViewItem("ABCD");
+            oItem1.Name = "ABCD";
+            //oItem1.Group = treeListView1.Groups[1];
+            treeListView1.Items.Add(oItem1);
+
+            TreeListViewItem oItem2 = new TreeListViewItem("PQR", 0);
+          //  oItem2.Group = treeListView1.Groups[1];
+            treeListView1.Items["ABCD"].Items.Add(oItem2);
+            treeListView1.Items["ABCD"].Items.Add("XYZ");
+
+
+        }
+
+        private void treeListView1_ItemActivate(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeListView1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(treeListView1.SelectedItem.Text);  
+        }
+    }
+}
