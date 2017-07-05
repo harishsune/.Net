66226d6bcfffb97f06aae674464310af9ab14e70
harishsune <harishsune@hotmail.com>
2017-07-05 23:52:06 +0530

    CheckBoxCombo

---
+++ CheckComboBoxTest/CheckComboBoxTest/Form1.Designer.cs
@@ -0,0 +1,62 @@
+namespace CheckComboBoxTest {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && ( components != null )) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.ccb = new CheckComboBoxTest.CheckedComboBox();
+            this.SuspendLayout();
+            // 
+            // ccb
+            // 
+            this.ccb.CheckOnClick = true;
+            this.ccb.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.ccb.DropDownHeight = 1;
+            this.ccb.FormattingEnabled = true;
+            this.ccb.IntegralHeight = false;
+            this.ccb.Location = new System.Drawing.Point(12, 22);
+            this.ccb.Name = "ccb";
+            this.ccb.Size = new System.Drawing.Size(382, 21);
+            this.ccb.TabIndex = 0;
+            this.ccb.ValueSeparator = ", ";
+          //  this.ccb.DropDownClosed += new System.EventHandler(this.ccb_DropDownClosed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 306);
+            this.Controls.Add(this.ccb);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "CheckedComboBox Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CheckedComboBox ccb;
+    }
+}
+
